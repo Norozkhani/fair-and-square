480338297893fe547fc32613ce54eafba1fd609f
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-03-06 21:35:42 +0100

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,65 @@
-"use client"
+/** @format */
+
+"use client";
 
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "./globals.css";
 import { useEffect } from "react";
 
-
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+	subsets: ["latin"],
+});
 
 export default function Home() {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
+	useEffect(() => {
+		require("bootstrap/dist/js/bootstrap.bundle.min.js");
+	}, []);
 	return (
-	<>
-   <div onClick={() => {
-     console.log('yes')
-   }} className="d-flex 
+		<>
+			<div
+				onClick={() => {
+					console.log("yes");
+				}}
+				className="d-flex 
 justify-content-center align-items-center">
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
+				<button
+					type="button"
+					className="btn btn-primary"
+					data-bs-toggle="modal"
+					data-bs-target="#exampleModal">
+					Launch demo modal cheking
+					whether I succeeded in
+					cloning(sammani)
+				</button>
 
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Modal title
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">...</div>
-          </div>
-        </div>
-      </div>
-    </div></>
+				<div
+					className="modal fade"
+					id="exampleModal"
+					tabIndex="-1"
+					aria-labelledby="exampleModalLabel"
+					aria-hidden="true">
+					<div className="modal-dialog">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5
+									className="modal-title"
+									id="exampleModalLabel">
+									Modal title
+								</h5>
+								<button
+									type="button"
+									className="btn-close"
+									data-bs-dismiss="modal"
+									aria-label="Close"></button>
+							</div>
+							<div className="modal-body">
+								...
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
