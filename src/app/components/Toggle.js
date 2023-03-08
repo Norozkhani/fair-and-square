293a4fed7eb92e7fd2983e9ae67e04f239e5f603
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-03-08 11:45:24 +0100

    Dropdown

--- src/app/components/Toggle.js
+++ src/app/components/Toggle.js
@@ -1,23 +1,47 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 
-const Toggle = ({setToggleHandler}) => {
-	let [toggle, setToggle] =useState(false)
-	const onClickHandler = (e) => {
-		setToggle(()=> {
-			setToggleHandler(e.target.innerHTML)
-			
-		})
-	}
-	return(
-		<>
-		<div>
-			<button onClick={onClickHandler}>gender</button>
-			<button onClick={onClickHandler}>religion</button>
-			<button onClick={onClickHandler}>sexuality</button>
-		</div>
-		</>
-	)
-}
+const Toggle = ({ setToggleHandler }) => {
+  let [toggle, setToggle] = useState(false);
+  const onClickHandler = (e) => {
+    setToggle(() => {
+      setToggleHandler(e.target.innerHTML);
+    });
+  };
+  return (
+    <>
+      <div className="container vh-3">
+        <div className="dropdown m-3">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            id="dropdownMenuButton1"
+            aria-expanded="false"
+          >
+            Category
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+              <a className="dropdown-item" onClick={onClickHandler}>
+                gender
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" onClick={onClickHandler}>
+                religion
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" onClick={onClickHandler}>
+                sexuality
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Toggle
+export default Toggle;
