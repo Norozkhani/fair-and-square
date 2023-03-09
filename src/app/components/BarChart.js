c36ea3dcccbe1eb3a549c9764d7b1b9abe974c96
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-03-09 16:37:32 +0100

    Fixing the toggle button

--- src/app/components/BarChart.js
+++ src/app/components/BarChart.js
@@ -1,63 +1,83 @@
+/** @format */
+
 import { useEffect } from "react";
 import { Chart } from "chart.js";
 import DATA from "../../../DiagramData.json";
 import styles from "./chart.module.css";
-function Example({ toggleHandler }) {
-  const catagory = DATA[toggleHandler].map((elem) => {
-    return elem.catagory;
-  });
-  const amount = DATA[toggleHandler].map((elem) => {
-    return elem.amount;
-  });
+import Toggle from "./Toggle";
+import { useState } from "react";
+function Example() {
+	const [toggleTrait, setToggleTrait] =
+		useState("gender");
+	console.log(toggleTrait);
+	const catagory = DATA[
+		toggleTrait
+	].map((elem) => {
+		return elem.catagory;
+	});
+	const amount = DATA[toggleTrait].map(
+		(elem) => {
+			return elem.amount;
+		}
+	);
 
-  useEffect(() => {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: catagory,
-        datasets: [
-          {
-            data: amount,
-            label: catagory,
-            backgroundColor: [
-              "#3A1078",
-              "#4E31AA",
-              "#2F58CD",
-              "#3795BD",
-              "#307383",
-              "#4DB892",
-            ],
-            borderWidth: 1,
-            barThickness: 35,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
-  }, [toggleHandler]);
+	useEffect(() => {
+		var ctx = document
+			.getElementById("myChart")
+			.getContext("2d");
+		var myChart = new Chart(ctx, {
+			type: "bar",
+			data: {
+				labels: catagory,
+				datasets: [
+					{
+						data: amount,
+						label: catagory,
+						backgroundColor: [
+							"#3A1078",
+							"#4E31AA",
+							"#2F58CD",
+							"#3795BD",
+							"#307383",
+							"#4DB892",
+						],
+						borderWidth: 1,
+						barThickness: 35,
+					},
+				],
+			},
+			options: {
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								beginAtZero: true,
+							},
+						},
+					],
+				},
+			},
+		});
+	}, [toggleTrait]);
 
-  return (
-    <>
-      {/* Bar chart */}
-      <div className=" w-50 mx-auto my-auto">
-        <h1 className="">Bar Chart</h1>
-        <div>
-          <canvas className={styles.Chart} id="myChart"></canvas>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			{/* Bar chart */}
+			<div className=" w-50 mx-auto my-auto">
+				<h1 className="">Bar Chart</h1>
+				<div>
+					<Toggle
+						setToggleTrait={
+							setToggleTrait
+						}
+					/>
+					<canvas
+						className={styles.Chart}
+						id="myChart"></canvas>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Example;
--- src/app/page.js
+++ src/app/page.js
@@ -6,25 +6,21 @@ import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "./globals.css";
 import { useEffect } from "react";
-import BarChart from "./components/BarChart"
-import Toggle from "./components/Toggle";
+import BarChart from "./components/BarChart";
+
 import { useState } from "react";
 
 const inter = Inter({
 	subsets: ["latin"],
 });
 
-
 export default function Home() {
-  const [toggleHandler, setToggleHandler] = useState("gender")
 	useEffect(() => {
 		require("bootstrap/dist/js/bootstrap.bundle.min.js");
 	}, []);
 	return (
 		<>
-    <Toggle setToggleHandler={setToggleHandler}/>
-    <BarChart toggleHandler={toggleHandler}/>
-
+			<BarChart />
 		</>
 	);
 }
