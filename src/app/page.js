919e752b039d3d903369e4b3896aa2e4fabedb91
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-03-10 13:29:26 +0100

    created sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,25 @@
 
 "use client";
 
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "./globals.css";
 import { useEffect } from "react";
 import BarChart from "./components/BarChart";
+import Sidebar from "./components/Sidebar";
 
-import { useState } from "react";
-
-const inter = Inter({
-	subsets: ["latin"],
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
 });
 
 export default function Home() {
-	useEffect(() => {
-		require("bootstrap/dist/js/bootstrap.bundle.min.js");
-	}, []);
-	return (
-		<>
-			<BarChart />
-		</>
-	);
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  return (
+    <>
+      <BarChart />
+    </>
+  );
 }
